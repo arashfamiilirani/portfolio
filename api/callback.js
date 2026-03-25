5e5ede319de62f96f1551271b364e4276c58f471
Arash Famiilirani <arash.fa.ir@gmail.com>
2026-03-25 13:20:01 -0400

    Update callback.js

--- api/callback.js
+++ api/callback.js
@@ -10,6 +10,8 @@ export default async function handler(req, res) {
     }),
   });
   const data = await response.json();
+  
+  // This section below is what we are fixing:
   res.send(`
     <script>
       window.opener.postMessage("authorization:github:success:${JSON.stringify(data)}", "*");
