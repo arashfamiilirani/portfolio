9b25ba16816a64f2aabcac6e0ea3a9880803278d
Arash Famiilirani <arash.fa.ir@gmail.com>
2026-03-25 12:06:02 -0400

    Create callback.js

---
+++ api/callback.js
@@ -0,0 +1,19 @@
+export default async function handler(req, res) {
+  const { code } = req.query;
+  const response = await fetch("https://github.com/login/oauth/access_token", {
+    method: "POST",
+    headers: { "Content-Type": "application/json", Accept: "application/json" },
+    body: JSON.stringify({
+      client_id: process.env.GITHUB_CLIENT_ID,
+      client_secret: process.env.GITHUB_CLIENT_SECRET,
+      code,
+    }),
+  });
+  const data = await response.json();
+  res.send(`
+    <script>
+      window.opener.postMessage("authorization:github:success:${JSON.stringify(data)}", "*");
+      window.close();
+    </script>
+  `);
+}
